78831ccada891e29f1ad90a90520ac0272d6d499
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-04-03 18:55:34 -0400

    Changing style and class with js

---
+++ week3/changingStyleAndClass/index1.js
@@ -0,0 +1,17 @@
+
+
+var newH2 = document.createElement('h2')
+document.body.appendChild(newH2)
+newH2.textContent = [
+"This is H2 content",
+"This is H2 content",
+"This is H2 content",
+"This is H2 content",
+"This is H2 content",
+]
+
+
+// newH2.style.fontSize = 20-px;
+// newH2.style.FontWeight = lighter;
+// newH2.style.fontFamily = sans-serif;
+newH2.style.color = "cornflowerblue";
