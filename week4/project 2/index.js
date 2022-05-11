25123c2833842cd05b5853831e87c8255cbc7535
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-05-11 18:04:20 -0400

    project 2

--- week4/project 2/index.js
+++ week4/project 2/index.js
@@ -1,46 +1,53 @@
-var readlineSync = require("readline-sync");
-console.log("Welcome to Calculations 101");
-
-var num1 = readlineSync.questionInt("Please enter your first number:");
-var num2 = readlineSync.questionInt("Please enter your second number:");
-var userOperation = readlineSync.question("Please enter the operation to perform: (add, sub, mul, div)");
 
 function myAddition(numm1, numm2){
-    return numm1 + numm2;
+    return  Number(numm1) + Number(numm2);
+    
 }
+
 function mySubtraction(numm1, numm2){
-    return numm1 - numm2;
+    return Number(numm1) - Number(numm2);
 
 }
 function myMultiplication(numm1, numm2){
 
-    return numm1 * numm2;
+    return Number(numm1) * Number(numm2);
 
 }
 function myDivision(numm1, numm2){
-    return numm1 / numm2;
+    return Number(numm1) / Number(numm2);
 
 }
 
+document.getElementById("addButton").addEventListener("click", function(){
+   var firstnum = document.getElementById("firstNumber").value
+    var secondnum = document.getElementById("secondNumber").value
+   document.getElementById("add").textContent = "The result is " + myAddition (firstnum, secondnum)
+
+});
+
+document.getElementById("subtractButton").addEventListener("click", function(){
+    var thirdnum =  document.getElementById("thirdNumber").value
+    var fourthnum = document.getElementById("fourthNumber").value
+   document.getElementById("subtract").textContent = "The result is " + mySubtraction (thirdnum, fourthnum)
+
+});
+
+document.getElementById("multiplyButton").addEventListener("click", function(){
+    var fifthnum =  document.getElementById("fifthNumber").value
+    var sixthnum = document.getElementById("sixthNumber").value
+   document.getElementById("multiply").textContent = "The result is " + myMultiplication (fifthnum, sixthnum)
+
+});
+
+document.getElementById("divideButton").addEventListener("click", function(){
+    var seventhnum =  document.getElementById("seventhNumber").value
+    var eighthnum = document.getElementById("eighthNumber").value
+   document.getElementById("divide").textContent = "The result is " + myDivision (seventhnum, eighthnum)
+
+});
 
 
-function calculatedResult(number1, number2, Operation){
-switch(Operation){
-    case "add":
-    console.log("The result is:" + myAddition(number1, number2));
-    break;
-    
-    case "sub":
-        console.log("The result is:" + mySubtraction(number1, number2));
-    break;
-    
-    case "mul":
-            console.log("The result is:" + myMultiplication(number1, number2));
-             
-    break;
-    
-              default: "div"
-        console.log("The result is:" + myDivision(number1, number2));
-}
-}    
-calculatedResult(num1, num2,userOperation);
+document.getElementById("multiplyButton").addEventListener("click", myMultiplication);
+
+document.getElementById("divideButton").addEventListener("click", myDivision);
+
