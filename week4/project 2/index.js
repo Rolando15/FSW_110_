153204151c740ee95111ab809492d0ddbda54420
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-05-11 19:28:08 -0400

    updated project 2

--- week4/project 2/index.js
+++ week4/project 2/index.js
@@ -31,14 +31,14 @@ document.getElementById("subtractButton").addEventListener("click", function(){
    document.getElementById("subtract").textContent = "The result is " + mySubtraction (thirdnum, fourthnum)
 
 });
-
+document.getElementById("multiplyButton").addEventListener("click", myMultiplication);
 document.getElementById("multiplyButton").addEventListener("click", function(){
     var fifthnum =  document.getElementById("fifthNumber").value
     var sixthnum = document.getElementById("sixthNumber").value
    document.getElementById("multiply").textContent = "The result is " + myMultiplication (fifthnum, sixthnum)
 
 });
-
+document.getElementById("divideButton").addEventListener("click", myDivision);
 document.getElementById("divideButton").addEventListener("click", function(){
     var seventhnum =  document.getElementById("seventhNumber").value
     var eighthnum = document.getElementById("eighthNumber").value
@@ -47,7 +47,40 @@ document.getElementById("divideButton").addEventListener("click", function(){
 });
 
 
-document.getElementById("multiplyButton").addEventListener("click", myMultiplication);
 
-document.getElementById("divideButton").addEventListener("click", myDivision);
 
+
+
+document.getElementById("addButton").style.backgroundColor = "orange"
+document.getElementById("subtractButton").style.backgroundColor = "orange"
+document.getElementById("multiplyButton").style.backgroundColor = "orange"
+document.getElementById("divideButton").style.backgroundColor = "orange"
+
+document.getElementById("add").style.padding = "25px";
+document.getElementById("subtract").style.padding = "25px";
+document.getElementById("multiply").style.padding = "25px";
+document.getElementById("divide").style.padding = "25px";
+
+
+document.getElementById("add").style.border = "thick dotted purple";
+document.getElementById("subtract").style.border = "thick dotted purple";
+document.getElementById("multiply").style.border = "thick dotted purple";
+document.getElementById("divide").style.border = "thick dotted purple";
+
+
+document.getElementById("firstNumber").style.textAlign = "center"
+document.getElementById("secondNumber").style.textAlign = "center"
+document.getElementById("thirdNumber").style.textAlign = "center"
+document.getElementById("fourthNumber").style.textAlign = "center"
+document.getElementById("fifthNumber").style.textAlign = "center"
+document.getElementById("sixthNumber").style.textAlign = "center"
+document.getElementById("seventhNumber").style.textAlign = "center"
+document.getElementById("eighthNumber").style.textAlign = "center"
+
+
+document.getElementById("add").style.margin = "50px 10px 20px 30px";
+document.getElementById("subtract").style.margin = "50px 10px 20px 30px";
+document.getElementById("multiply").style.margin = "50px 10px 20px 30px";
+document.getElementById("divide").style.margin = "50px 10px 20px 30px";
+
+document.body.style.backgroundColor = "lightgreen"
