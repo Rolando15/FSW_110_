d66fef075de961b85e6aeb487c301d5305af82c4
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-05-25 16:16:50 -0400

    files

--- week6/change/click/app.js
+++ week6/change/click/app.js
@@ -1,1 +1,9 @@
 // make the box disapear when the user clicks it
+
+var x = document.getElementsByClassName("red-box")[0];
+
+function myFunction() {
+  x.style.display = "none";
+}
+ x.addEventListener("click" , myFunction)
+ 
