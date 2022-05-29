e2edd10389a1e683a3bdeb6fd3f0bc215e7ef7d5
Rolando15 <rolando.hernandez@bryanuniversity.edu>
2022-05-29 14:14:38 -0400

    mouseover

--- week6/change/click/keypress/mousemove/app.js
+++ week6/change/click/keypress/mousemove/app.js
@@ -1,7 +1,9 @@
-
-
-var box = document.getElementsByClassName("red-box") [0];
-var ctx = canvas.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+function showCoords(event) {
+    document.body.getElementsByClassName("red-box") [0];
+    document.body.getElementsByClassName("red-box")[0].addEventListener(onMouseOver).showCoords(event)
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    showCoords(event)
+    console.log(coords)
+  }
